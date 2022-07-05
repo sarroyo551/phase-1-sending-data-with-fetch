67a79f1732862b05be12058feca238f32ecedfd6
Santi Arroyo <santi.arroyo551@gmail.com>
2022-07-05 13:45:20 -0400

    commit tree 526015e114a169e3759375a637b2b3bb885b5ff6

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 // Add your code here
+/*const configurationObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+        dogName: 'Byron',
+        dogBreed: 'Poodle',
+    }),
+}
+
+fetch('http://localhost:3000/dogs', configurationObject);*/
+
+
+/*fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  }),
+});*/
+
+/*const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+        alert('Bad things! Ragnarők!')
+        console.log(error.message)
+    })
+*/
+
+    function submitData (name, email) {
+      return fetch("http://localhost:3000/users", {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name:name,
+          email:email,
+        }),
+        method: "POST",
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (newUser) {
+          const body = document.querySelector('body')
+          body.append(newUser.id)
+        })  
+        .catch(function (error) {
+          const body = document.querySelector('body')
+          body.append(error.message)
+      })
+    }
